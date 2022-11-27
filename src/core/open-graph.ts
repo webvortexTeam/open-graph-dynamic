99095a8a4a854fd0e9840a08684ed9d4ea0583a2
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-27 19:41:55 +0200

    feat: create open graph

---
+++ src/core/open-graph.ts
@@ -0,0 +1,30 @@
+import http from 'http';
+import { env } from '~/shared/env';
+import { validationOpenGraphProps } from '~/shared/validators';
+import { openGraphInputType, openGraphOutputType } from './protocols';
+import queryString from 'query-string';
+
+export const openGraph = async (props: openGraphInputType): Promise<openGraphOutputType> => {
+  try {
+    const isValid = await validationOpenGraphProps(props);
+    const data: queryString.StringifiableRecord = {
+      ...isValid,
+      ...isValid.user,
+      user: '',
+    };
+
+    const source = queryString.stringifyUrl(
+      {
+        url: `${env.baseUrl}`,
+        query: { ...data },
+      },
+      {
+        skipEmptyString: true,
+      },
+    );
+
+    return { source };
+  } catch (error) {
+    throw error;
+  }
+};
